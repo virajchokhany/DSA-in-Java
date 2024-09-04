579adfec7980802b31fe6d721649c04350a3801c
Viraj Chokhany <viraj.chokhany@gmail.com>
2024-09-05 00:52:55 +0530

    Dp LIS category

--- DP/LIS.java
+++ DP/LIS.java
@@ -1,5 +1,8 @@
 package DP;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class LIS {
     public static void display(int a[]){
         for(int val : a){
@@ -115,12 +118,84 @@ public class LIS {
         }
         return ans;
     }
-    public static void main(String[] args) {
-        int a[]={0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15,14};
+    
+    // minimum deletion required to make array sorted
+
+    public static int minDeletion(int a[]){
         int dp[]=new int[a.length];
-        int mxLen = 0;
-        mxLen = lengthOfLIS_tabu(a, dp);
-        display(dp);
-        System.out.println(mxLen);
+        int mx = -(int)1e9;
+        for(int i=0;i<a.length;i++){
+            for(int j=i-1;j>=0;j--){
+                if(a[i]>=a[j]){
+                    dp[i]=Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i]++;
+            mx = Math.max(mx,dp[i]);
+        }
+        return a.length-mx;
+    }
+    static class Pair{
+        int x,y;
+        public Pair(int x,int y) {
+            this.x=x;
+            this.y=y;
+        }
+        public Pair() {
+        }
+    }
+    public static int maxBridges(int a[],int b[]){
+        Pair p[]=new Pair[a.length];
+        for(int i=0;i<a.length;i++){
+            p[i]=new Pair(a[i],b[i]);
+        }
+        
+        Arrays.sort(p,(x,y)->{
+            return x.x-y.x;
+        });
+
+        int dp[]=new int[a.length];
+        int ans = 0;
+        for(int i=0;i<a.length;i++){
+            for(int j=i-1;j>=0;j--){
+                if(p[i].x>p[j].x && p[i].y>p[j].y){
+                    dp[i]= Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i]++;
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes,(a,b)->{
+            return a[0]-b[0];
+        });
+        int dp[]=new int[envelopes.length];
+        int ans = 0;
+        for(int i=0;i<envelopes.length;i++){
+            for(int j=i-1;j>=0;j--){
+                if(envelopes[i][0] >  envelopes[j][0] && envelopes[i][1] > envelopes[j][1]){
+                    dp[i] = Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i]++;
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        // int a[]={0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15,14};
+        // int dp[]=new int[a.length];
+        // int mxLen = 0;
+        // mxLen = lengthOfLIS_tabu(a, dp);
+        // display(dp);
+        // System.out.println(mxLen);
+        // System.out.println(minDeletion(new int[]{1,3,2,2,2,2}));
+        int a[]={8,1,4,3,5,2,6,7};
+        int b[]={1,2,3,4,5,6,7,8};
+        // int a[]={6,4,2,1};
+        // int b[]={2,3,6,5};
+        System.out.println(maxBridges(a, b));
     }
 }
