ef1a4dff536ddf0e7b296ff905a109f5d7c57aef
Viraj Chokhany <viraj.chokhany@gmail.com>
2025-03-24 18:23:16 +0530

    Heap Implementation

--- Heap/client.java
+++ Heap/client.java
@@ -1,13 +1,10 @@
 public class client {
     public static void main(String[] args) throws Exception{
         int arr[]={2,0,-1,-9,7,10};
-        heap h = new heap(false);
-        for(int i=0;i<arr.length;i++)
-            h.add(arr[i]);
-        // h.display();
-
+        heap h = new heap(arr,false);
         while(h.size()>0){
             System.out.print(h.remove()+" ");
         }
+        System.out.println(h.arr);
     }
 }
